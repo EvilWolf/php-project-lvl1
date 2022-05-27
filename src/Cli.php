37c026ed14af1b7d23bac529e24e154fbfed5ebb
Ilya Bogatov <wolfofdeath@yandex.ru>
2022-05-27 21:12:48 +0300

    Add dev package squizlabs/php_codesniffer, add linter to makefile, fix liter errors in package

--- src/Cli.php
+++ src/Cli.php
@@ -5,11 +5,9 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greeting() {
+function greeting()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
-
-
-
